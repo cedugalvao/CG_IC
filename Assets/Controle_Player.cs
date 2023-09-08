f41dbc507702bce9f138abdc39ac7ad950176e07
Carlos Eduardo N. T. Galvao <centg@ic.ufal.br>
2023-09-08 13:14:57 -0300

    escada e player pula

--- Assets/Controle_Player.cs
+++ Assets/Controle_Player.cs
@@ -12,23 +12,77 @@ public class NewBehaviourScript1 : MonoBehaviour{
 
     public float velocidadeAndar;
     public float velocidadeCorrida;
+    public float velocidadePulo;
+
+    [Header("Executando")]
+    Vector3 novaVelocidade;
+    bool noChao = false;
+    bool pulando = false;
+
 
 
     // Start is called before the first frame update
     void Start(){
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update(){
-        
-    }
-    private void FixedUpdate(){
-        Vector3 novaVelocidade = Vector3.up * rb.velocity.y;
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * 3f);
+
+        novaVelocidade = Vector3.up * rb.velocity.y;
         float velocidade = Input.GetKey(KeyCode.LeftShift) ? velocidadeCorrida : velocidadeAndar;
         novaVelocidade.x = Input.GetAxis("Horizontal") * velocidade;
         novaVelocidade.z = Input.GetAxis("Vertical") * velocidade;
-        rb.velocity = novaVelocidade;
+        if (noChao)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && !pulando)
+            {
+                novaVelocidade.y = velocidadePulo;
+                pulando = true;
+            }
+        }
+        rb.velocity = transform.TransformDirection(novaVelocidade);
 
+    }
+    public static float RestrictAngle(float angle, float angleMin, float angleMax){
+        if (angle > 180)
+            angle -= 360;
+        else if (angle < -180)
+            angle += 360;
+
+        if (angle > angleMax)
+            angle = angleMax;
+        if (angle < angleMin)
+            angle = angleMin;
+
+        return angle;
+    }
+    void LateUpdate()
+    {
+        // Vertical rotation
+        Vector3 e = head.eulerAngles;
+        e.x -= Input.GetAxis("Mouse Y") * 2f;   //  Edit the multiplier to adjust the rotate speed
+        e.x = RestrictAngle(e.x, -85f, 85f);    //  This is clamped to 85 degrees. You may edit this.
+        head.eulerAngles = e;
+    }
+    void FixedUpdate(){
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 1f))
+        {
+            noChao = true;
+        }
+        else noChao = false;
+
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        noChao = true;
+        pulando = false;
+    }
+
+
+    void OnCollisionExit(Collision collision){
+        noChao = false;
     }
 }
